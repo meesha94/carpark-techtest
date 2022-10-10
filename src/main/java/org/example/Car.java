7e151c4e7a215f4a94a84d347c351b0f883a712c
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-10 11:31:32 +0100

    created car class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,37 @@
+package org.example;
+mport java.util.ArrayList;
+
+public class Car extends Vehicle {
+
+    public Car(){
+
+        regularSpacesNeeded = 1;
+        largeSpacesNeeded = 1;
+        size = VehicleType.Car;
+
+    }
+
+    @Override
+    public boolean canFitLargeSpace(ParkingSpace largeSpace) {
+
+        return true;
+    }
+
+    @Override
+    public boolean canFitRegularSpace(ParkingSpace regularSpace) {
+
+        return true;
+    }
+
+    @Override
+    public boolean canFitSmallSpace(ParkingSpace smallSpace) {
+
+        return false;
+    }
+
+};
+
+
+
+
+
